d6ae40a7b26e4a35604e9a7d996e1f85507df2bb
Sufian681 <sufiansonic@gmail.com>
2026-09-23 19:42:14 +0900

    Add files via upload

---
+++ A2.py
@@ -0,0 +1,14 @@
+n = int(input("Enter a number (try 5 or 12): "))
+guess = input("Left shift doubles it. Guess: " + str(n) + " << 1 = ? ")
+
+input("NOT - flips every bit. Press enter ")
+print(" 12 =", bin(12)[2:])
+
+input("XOR - different bits give 1. Press Enter ")
+print(" 12 ^ 10 =", 12 ^ 10)
+
+input("Left shift - multiplies by 2. Press Enter ")
+print(" ", n, "<< 1 =", n << 1, " your guess:", guess)
+
+input("Right shift - divides by 2. Press enter")
+print(" ", n, ">> 1 =", n >> 1)
